c796e9edd53a868a45674a72da13ca6a80ba0c55
alantoledo007 <alantoledo007@gmail.com>
2020-10-17 15:29:53 -0300

    disable Dimensions for default testing

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ const styles = require('./styles');
 
 module.exports = function useBootnative (custom = {vars:{},colors:{}}) {
     return function(style){
-        const { Dimensions } = require('react-native');
-        const dimensions = Dimensions.get('window');
-        //const dimensions = {width:1024, height:0};
+        //const { Dimensions } = require('react-native');
+        //const dimensions = Dimensions.get('window');
+        const dimensions = {width:1024, height:0};
     return styleParser(style, styles(dimensions,custom), dimensions)
   };
 }
